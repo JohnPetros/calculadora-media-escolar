b3d959a51505b45e6e0e9b15d744426b573f20dd
João Pedro <joaopedro.nc@outlook.com>
2023-01-08 10:11:31 -0300

    ✨ feat: add PHP run time

--- api/index.php
+++ api/index.php
@@ -51,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calcula Média</title>
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
 </head>
 
